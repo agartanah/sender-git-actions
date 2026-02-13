ab8414c7f340f1d20f6f05ab04c2f46df1a052ff
agartanah <vyatin2017@mail.ru>
2026-02-13 16:16:59 +0700

    feat: add response entity for http response

--- webhook-receiver/src/main/java/ru/gnivc/sender/Controllers/WebhookController.java
+++ webhook-receiver/src/main/java/ru/gnivc/sender/Controllers/WebhookController.java
@@ -1,6 +1,8 @@
 package ru.gnivc.sender.Controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.gnivc.sender.Services.ProducerService;
 
@@ -13,36 +15,59 @@ public class WebhookController {
         this.producerService = producerService;
     }
 
-    @PostMapping("/github")
-    public String handleActionGithub(@RequestHeader("X-GitHub-Event") String event,
-                                     @RequestBody JsonNode body) {
+    @PostMapping("/cicd")
+    public ResponseEntity<String> handleCiCdEvent(
+            @RequestHeader("CI-Event-Type") String event,
+            @RequestBody JsonNode body
+    ) {
         try{
-            producerService.sendAction("github", event, body);
-            return "Event " + event + "!!!";
+            producerService.sendAction("cicd", event, body);
+            return new ResponseEntity<>("Event accepted", HttpStatus.OK);
         } catch (Exception e) {
-            return "Event Error";
+            e.printStackTrace();
+            return new ResponseEntity<>("Event error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/github")
+    public ResponseEntity<String> handleActionGithub(
+            @RequestHeader("X-GitHub-Event") String event,
+            @RequestBody JsonNode body) {
+        try {
+            producerService.sendAction("github", event, body);
+            return ResponseEntity.ok("Event " + event + " accepted");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Event error: " + e.getMessage());
         }
     }
 
     @PostMapping("/gitlab")
-    public String handleActionGitlab(@RequestHeader("X-GitLab-Event") String event,
-                                     @RequestBody JsonNode body) {
-        try{
-//            producerService.sendAction("gitlab", event, body);
-            return "In dev";
+    public ResponseEntity<String> handleActionGitlab(
+            @RequestHeader("X-GitLab-Event") String event,
+            @RequestBody JsonNode body) {
+        try {
+            producerService.sendAction("gitlab", event, body);
+            return ResponseEntity.ok("Event " + event + " accepted");
         } catch (Exception e) {
-            return "Event Error";
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Event error: " + e.getMessage());
         }
     }
 
     @PostMapping("/gitflic")
-    public String handleActionGitFlic(@RequestHeader("X-GitFlic-Event") String event,
-                                     @RequestBody JsonNode body) {
-        try{
-//            producerService.sendAction("gitflic", event, body);
-            return "In dev";
+    public ResponseEntity<String> handleActionGitFlic(
+            @RequestHeader("X-GitFlic-Event") String event,
+            @RequestBody JsonNode body) {
+        try {
+            producerService.sendAction("gitflic", event, body);
+            return ResponseEntity.ok("Event " + event + " accepted");
         } catch (Exception e) {
-            return "Event Error";
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Event error: " + e.getMessage());
         }
     }
 }
