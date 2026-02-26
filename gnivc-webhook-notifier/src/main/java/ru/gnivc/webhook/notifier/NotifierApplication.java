f0e8825c294a3ac754c0ca2f70eb7b494d921900
Harsat <mimkal@mail.ru>
2026-02-26 10:52:23 +0700

    Enable configuration fix

--- gnivc-webhook-notifier/src/main/java/ru/gnivc/webhook/notifier/NotifierApplication.java
+++ gnivc-webhook-notifier/src/main/java/ru/gnivc/webhook/notifier/NotifierApplication.java
@@ -2,7 +2,10 @@ package ru.gnivc.webhook.notifier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.gnivc.webhook.notifier.configuration.TelegramConfig;
 
+@EnableConfigurationProperties(TelegramConfig.class)
 @SpringBootApplication
 public class NotifierApplication {
     public static void main(String[] args){
